84a2729f9a465df75a098c78d5a9aabb960f9491
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2025-01-25 03:27:26 +0530

    Add support for ES module path resolution in index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
